cfcc27208d7085dd2dfb7011eec84963b8c554f5
Dan Bruder <danbruder@hey.com>
2026-01-03 20:14:25 -0500

    update formula

--- Formula/trello-cli.rb
+++ Formula/trello-cli.rb
@@ -1,7 +1,7 @@
 class TrelloCli < Formula
   desc "A comprehensive Trello CLI tool for humands and LLMs"
   homepage "https://trello-cli.netlify.app"
-  version "1.2.0"
+  version "1.3.0"
   license "MIT"
   head "https://github.com/danbruder/trello-cli.git", branch: "main"
 
@@ -9,11 +9,11 @@ class TrelloCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/danbruder/trello-cli/releases/download/v1.2.0/trello-cli-darwin-arm64"
-      sha256 "143f4c7577e0b1ce52311a51aaf391963ed19321e47c6b94b1881e93ca40013e"
+      url "https://github.com/danbruder/trello-cli/releases/download/v1.3.0/trello-cli-darwin-arm64"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/danbruder/trello-cli/releases/download/v1.2.0/trello-cli-darwin-amd64"
-      sha256 "1c6d6e5d279400412509df796b9eeb1cbc8d3abda50540da4e07c47f3a310534"
+      url "https://github.com/danbruder/trello-cli/releases/download/v1.3.0/trello-cli-darwin-amd64"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
 
     def install
@@ -23,11 +23,11 @@ class TrelloCli < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/danbruder/trello-cli/releases/download/v1.2.0/trello-cli-linux-arm64"
-      sha256 "f0748204f5d877c3ef3db382586c3a497bf1f3cee6d4ebb552fda4435fd04a87"
+      url "https://github.com/danbruder/trello-cli/releases/download/v1.3.0/trello-cli-linux-arm64"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/danbruder/trello-cli/releases/download/v1.2.0/trello-cli-linux-amd64"
-      sha256 "c0d378a50bde3f6275fe40bf76b17ccc40b6f21c814f2d9d7c90c567fa99dfb4"
+      url "https://github.com/danbruder/trello-cli/releases/download/v1.3.0/trello-cli-linux-amd64"
+      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
 
     def install
